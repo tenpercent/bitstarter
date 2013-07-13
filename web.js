f08052d6bb25cc5c8a1425e548f21b61e25364a4
tenpercent <max.podkorytoff@yandex.ru>
2013-07-13 12:48:38 +0000

    create

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+	var buf = fs.readFileSync('index.html');
+	var data = buf.toString();
+  response.send(data);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
